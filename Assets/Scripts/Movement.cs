7c899917e593b1a4e2db1813da92f55a7a6d4818
Hue Beholder <elfengirox00@gmail.com>
2017-11-27 12:34:54 -0300

    Animaciones Reparadas
    asdf

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidBody;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator> ();
+		playerRigidBody = GetComponent<Rigidbody> ();
+
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Animating (h, v);
+
+	}
+
+	void Move( float h, float v)
+	{
+		movement.Set (h, 0f, v);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidBody.MovePosition (transform.position + movement);
+	}
+
+	void Animating(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("IsWalking", walking);
+	}
+}
