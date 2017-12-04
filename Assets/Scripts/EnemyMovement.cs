796f6f25b7adc93462d91b9ef4dac612ac7259eb
HokeRompeorli <Hamechrs@gmail.com>
2017-12-04 15:50:53 -0300

    BGM
    BGM

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,9 +5,9 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour {
 
 	Transform player;   
-	Transform player2; // Reference to the player's position.
+	// Reference to the player's position.
 	Health1 playerHealth; 
-	Health1 playerHealth2; // Reference to the player's health.
+	 // Reference to the player's health.
 	Health enemyHealth;        // Reference to this enemy's health.
 	NavMeshAgent nav;               // Reference to the nav mesh agent.
 	Animator anim;
@@ -16,9 +16,9 @@ public class EnemyMovement : MonoBehaviour {
 	{
 		// Set up the references.
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
-		player2 = GameObject.FindGameObjectWithTag ("Player").transform;
+
 		playerHealth = player.GetComponent <Health1> ();
-		playerHealth2 = player.GetComponent <Health1> ();
+
 		enemyHealth = GetComponent <Health> ();
 		nav = GetComponent <NavMeshAgent> ();
 		anim = GetComponent<Animator> ();
@@ -42,20 +42,7 @@ public class EnemyMovement : MonoBehaviour {
 			nav.enabled = false;
 
 		}
-		/*if(enemyHealth.currentHealth > 0 && playerHealth2.currentHealth > 0)
-		{
-			// ... set the destination of the nav mesh agent to the player.
-			anim.SetBool("IsWalking",true);
-			nav.SetDestination (player2.position);
-		}
-		// Otherwise...
-		else
-		{
-			// ... disable the nav mesh agent.
-			nav.enabled = false;
 
-		}
-		*/
 	} 
 
 }
