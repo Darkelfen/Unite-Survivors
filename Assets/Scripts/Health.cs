50bc372343079c2dfee36b64a8658bfaf4fe27b2
HokeRompeorli <Hamechrs@gmail.com>
2017-12-05 22:11:23 -0300

    Revert "Merge branch 'master' of https://github.com/Darkelfen/Unite-Survivors"
    This reverts commit 4254370e1ba49c76cb0276a773847911f561b645, reversing
    changes made to 03596ab2f7112a0dc87c79600f4d7b4d0f118776.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,19 +5,6 @@ using UnityEngine;
 public class Health : MonoBehaviour {
 	public const int maxHealth = 100;
 	public int currentHealth = maxHealth;
-	bool isDead = false;
-	Animator anim;  
-	Movement mobMovement;
-	Vector3 pos;
-
-	public GameObject meat;
-	public GameObject snakeSkin;
-	public GameObject bearSkin;
-
-	void Awake()
-	{
-		anim = anim = GetComponent <Animator> ();	
-	}
 
 	public void TakeDamage(int amount)
 	{
@@ -26,33 +13,6 @@ public class Health : MonoBehaviour {
 		{
 			
 			currentHealth = 0;
-			Dead ();
-		}
-	}
-
-	void Dead ()
-	{
-		if (!isDead) {
-			isDead = true;
-			anim.SetTrigger ("Dead");
-			mobMovement.enabled = false;
-			if (this.CompareTag("Snake")) {
-				pos = new Vector3 (this.transform.position.x + Random.Range (0.1f, 0.2f), this.transform.position.y, this.transform.position.z + Random.Range (0.1f, 0.2f));
-				Instantiate(meat, pos, Quaternion.identity);
-				pos = new Vector3 (this.transform.position.x + Random.Range (0.1f, 0.2f), this.transform.position.y, this.transform.position.z + Random.Range (0.1f, 0.2f));
-				Instantiate(snakeSkin, pos, Quaternion.identity);
-			}
-			if (this.CompareTag ("Bear")) {
-				for (int i = 0; i< Random.Range(2,5); i++)
-				{
-					pos = new Vector3 (this.transform.position.x + Random.Range (0.1f, 0.2f), this.transform.position.y, this.transform.position.z + Random.Range (0.1f, 0.2f));
-					Instantiate(meat, pos, Quaternion.identity);
-				}
-				pos = new Vector3 (this.transform.position.x + Random.Range (0.1f, 0.2f), this.transform.position.y, this.transform.position.z + Random.Range (0.1f, 0.2f));
-				Instantiate(meat, pos, Quaternion.identity);
-				Instantiate(bearSkin, pos, Quaternion.identity);
-			}
-			this.Destroy ();
 		}
 	}
 }
